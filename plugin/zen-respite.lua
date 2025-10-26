699bd51ff00576fa43ee7421f0e761fffb29077b
静叶知秋 <80163187+luo-y3@users.noreply.github.com>
2025-10-27 05:22:06 +0700

    Delete plugin directory

--- plugin/zen-respite.lua
+++
@@ -1,6 +0,0 @@
--- zen-respite.nvim/plugin/zen-respite.lua
-
--- Optional setup hook for lazy.nvim or packer
--- vim.api.nvim_create_user_command("ZenRespite", function()
---   vim.cmd("colorscheme zen-respite")
--- end, {})
